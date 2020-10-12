c2b44fa8e4be498bfc3865e467290168855ef167
Arahant <arahant.ashok@gmail.com>
2020-10-12 16:57:07 -0400

    [cso-009] word count

--- r-10.12/string_manipulation.c
+++ r-10.12/string_manipulation.c
@@ -1,5 +1,6 @@
 #include<stdio.h>
 #include<stdlib.h>
+#include<strings.h>
 
 #define MAX 1024
 
@@ -7,59 +8,63 @@ char *diff1, diff2;
 int ctr = 0;
 int diff_ctr = 0;
 
-char** parse_words(char *buf) {
-    char c;
-    char word[MAX];
-    char ** words;
-    int i = 0;
-    int j = 0;
+void print_words(char *words[MAX], int n) {
+    for(int i=0;i<n;i++)
+        puts(words[i]);
+}
+
+void extract_words(char buf[MAX]) {
+    char *stop = strchr(buf, '\0');
+    int id1 = (int) (stop - buf);
+    char *nl = strchr(buf, '\n');
+    int id2 = (int) (nl - buf);
+    int max = id1<id2?id1:id2;
+
+    char *words[MAX];
+    int i = -1;
+    int c = 0;
+    int a = 0;
     int w = 0;
 
-    while(i<MAX) {
-        c=buf[i];
-        if(c!=' ' || c!='\0') {
-            word[j++] = c;
+    while(++i<max) {
+        a = (int) buf[i];
+        if((a>=65 && a<=90) || (a>=97 && a<=122)) {
+            c++;
         }
-        if(j>0) {
-            word[j] = '\0';
-            j=0;
-            words+w = word;
-            w++;
+        else {
+            if(c>0) {
+                char *word = malloc(sizeof(char)*c);
+                strncpy(word,buf+i-c,c);
+                words[w++] = word;
+                word = NULL;
+                c=0;
+            }
         }
-        i++;
     }
-    return words;
+    if(w>0)
+        print_words(words,w);
 }
 
-void print_words(char **words) {
-    char *word;
-    int i=0;
-    while((word=words[i])!=NULL)
-        puts(word);
-}
-
-void diff(char *buf1, char *buf2) {
-    int i=0;
-    while(i<MAX) {
-        char c1 = buf1[i];
-        char c2 = buf2[i];
-        if(c1!=c2) {
-            diff_ctr = ctr;
-            diff1[i] = c1;
-            diff2[i] = c2;
-        }
-        ctr++;
-    }
-}
+// void diff(char *buf1, char *buf2) {
+//     int i=0;
+//     while(i<MAX) {
+//         char c1 = buf1[i];
+//         char c2 = buf2[i];
+//         if(c1!=c2) {
+//             diff_ctr = ctr;
+//             diff1[i] = c1;
+//             diff2[i] = c2;
+//         }
+//         ctr++;
+//     }
+// }
 
 int main(int n, char *args[]) {
     FILE *fp = fopen(args[1],"r");
     char buf[MAX];
+    bzero(buf,MAX);
     while(fgets(buf,MAX,fp)!=NULL) {
-        puts(buf);
-        // char **words = parse_words(buf);
-        // print_words(words);
-        // insert_inorder(words); // store words
+        extract_words(buf);
     }
     fclose(fp);
     return 0;
