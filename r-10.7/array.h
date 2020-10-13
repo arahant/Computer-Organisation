40e0f6124ec2b5a68cfda7400c0e60a25dd30fb3
Arahant <arahant.ashok@gmail.com>
2020-10-13 12:23:08 -0400

    [cso-010] removed 10.7 files

--- r-10.7/array.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-
-int array_sum(int *arr, int n);
-void array_cpy(int *dst, int *src, int n);
-void bubble_sort(int *arr, int n);
-int big_to_little_endian(char *arr);
--- r-10.7/ptr.h
+++
@@ -1,5 +0,0 @@
-#pragma once
-
-void set_to_five(int *ptr);
-void initialize_ptr(void **ptr);
-void swap_int(int *x, int *y);
