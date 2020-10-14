e112ca2283ca290449b23af18a5f20c96f60a424
Arahant <arahant.ashok@gmail.com>
2020-10-14 12:06:55 -0400

    [cso-010] main

--- r-10.14/main.c
+++ r-10.14/main.c
@@ -7,12 +7,12 @@
 
 struct diff* diff_list[MAX];
 
-int row = 0;
+int row = -1;
 int col = 0;
 int it = 0;
 
-bool ignore_case  = false;
-bool ignore_space = false;
+bool ignore_case  = true;
+bool ignore_space = true;
 bool ignore_blank = false;
 
 int str_cmp(char *buf1, char *buf2) {
@@ -30,6 +30,8 @@ int str_cmp_2(char *buf1, char *buf2) {
     while(i1<MAX && i2<MAX) {
         int a1 = (int) buf1[i1];
         int a2 = (int) buf2[i2];
+        i1++;
+        i2++;
         if(a1==0 || a1==10 || a2==0 || a2==10)
             break;
         if(ignore_space) {
@@ -41,34 +43,30 @@ int str_cmp_2(char *buf1, char *buf2) {
             if(a2>=97) a2 -= 32;
         }
         diff = a1-a2;
-        i1++;
-        i2++;
+        if(diff!=0) {
+            col = i1;
+            break;
+        }
     }
     return diff;
 }
 
-int str_diff(char *buf1, char *buf2) {
+int diff_col(char *buf1, char *buf2) {
     int i=0;
-    for(int i=0;i<MAX;i++)
-        if(buf1[i]!=buf2[i])
-            break;
+    while(i<MAX && buf1[i]==buf2[i])
+        i++;
     return i;
 }
 
 void compare(char buf1[MAX], char buf2[MAX]) {
-    for(int i=0;i<MAX;i++) {
-        if(str_cmp(buf1,buf2)!=0) {
-            struct diff *node = save_diff(col/1024,row/1024,buf1,buf2);
-            diff_list[it++] = node;
-            break;
-        }
-        row++;
-        col = str_diff(buf1,buf2);
+    if(str_cmp(buf1,buf2)!=0) {
+        struct diff *node = save_diff(col,row,buf1,buf2);
+        diff_list[it++] = node;
     }
 }
 
 void print() {
-    for(int i=0;i<MAX;i++)
+    for(int i=0;i<it;i++)
         print_diff(diff_list[i]);
 }
 
@@ -79,6 +77,8 @@ int main(int n, char *args[]) {
     bzero(buf1,MAX);
     bzero(buf2,MAX);
     while(fgets(buf1,MAX,fp1)!=NULL && fgets(buf2,MAX,fp2)!=NULL) {
+        row++;
+        col = diff_col(buf1,buf2);
         compare(buf1,buf2);
     }
     print();
