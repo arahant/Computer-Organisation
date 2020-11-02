9f76ef401654550ef813dfac8ca00b19159af041
Arahant <arahant.ashok@gmail.com>
2020-11-02 12:07:44 -0500

    [cso-009] array

--- r-11.2/main.c
+++ r-11.2/main.c
@@ -3,6 +3,7 @@
 // gcc main.c
 // objdump -d a.out
 
+#define MAX 10
 void swap(int *a, int *b) {
     int c = *a;
     *a = *b;
@@ -13,7 +14,14 @@ int fib(int n) {
     if(n<=1)
         return n;
 
-    return fib(n-1) + fib(n-2);
+    // return fib(n-1) + fib(n-2);
+    int a = fib(n-1); //%rbp-12
+    int b = fib(n-2);
+    return a+b;
+    // fib(n-1): %eax (n-1) -> %rbp-12 -> %ecx
+    // fib(n-2): %eax
+    // %ecx = %ecx + %eax
+
 }
 
 long compare(long x, long y) {
@@ -45,6 +53,13 @@ int loop(int n, int d, int g) {
     return res;
 }
 
+void array(int n) {
+    int arr[MAX];
+    for (int i=0;i<n;i++) {
+        arr[i] = i;
+    }
+}
+
 int main() {
     int a = 1;
     int b = 2;
@@ -53,5 +68,6 @@ int main() {
     loop(5,8,7);
     fib(5);
     boolean();
+    array(5);
     return 0;
 }
